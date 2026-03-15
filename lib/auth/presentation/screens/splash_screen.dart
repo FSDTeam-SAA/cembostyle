75d33574c41283ce57d475f7d574af82b7da7161
farhan-shahariar <farhan15-3460@diu.edu.bd>
2026-03-15 15:16:43 +0600

    Splash screen design complete.

---
+++ lib/auth/presentation/screens/splash_screen.dart
@@ -0,0 +1,19 @@
+import 'package:cembostyle/core/common/constants/app_images.dart';
+import 'package:cembostyle/core/common/widgets/app_scaffold.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return AppScaffold(body: Center(
+      child: Image.asset(AppImages.appLogo, width: 193, height: 167,),
+    ));
+  }
+}
